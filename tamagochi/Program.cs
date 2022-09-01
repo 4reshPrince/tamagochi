47dba0429a5732ed8d7855cbb641ba63f45986c1
Your Name <you@example.com>
2022-09-01 10:47:31 +0200

    tama

---
+++ tamagochi/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+Console.WriteLine("Welcome");
+
+Tamagotchi Tama = new Tamagotchi;
+
+Console.WriteLine("Enter a name of your Tamagotchi");
+Tama.name = Console.ReadLine();
+
+Console.WriteLine($" ööö {Tama.name} ööö");
